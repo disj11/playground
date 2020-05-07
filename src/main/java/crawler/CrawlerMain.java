b2b604e9c3b7adf64d3d673448c092f8ecd04035
disj11 <disj11@naver.com>
2020-05-07 12:06:04 +0900

    예제용 크롤링 도메인 변경

--- src/main/java/crawler/CrawlerMain.java
+++ src/main/java/crawler/CrawlerMain.java
@@ -4,7 +4,7 @@ public class CrawlerMain {
     public static void main(String[] args) {
         try (WebDriverBase driverBase = new WebDriverBase()) {
             AnchorImageCrawler crawler = new AnchorImageCrawler(driverBase);
-            ImageCrawlingOptions options = new ImageCrawlingOptions("https://www.facebook.com/phymongshe.official/");
+            ImageCrawlingOptions options = new ImageCrawlingOptions("https://www.naver.com");
             crawler.start(options, result -> System.out.println("src : " + result.getImgSrc() + ", href : " + result.getAnchorHref()));
         }
     }
